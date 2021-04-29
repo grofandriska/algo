3cca495ca5d55f29168ce56505bb569d52fb8526
Gróf András <andrasgrof93@gmail.com>
2021-04-29 10:19:09 +0200

    algo feladat

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,18 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.isEmpty()) {
+            return -1;
+        }
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                String tmp = input.substring(i, i + 1);
+                result += Integer.parseInt(tmp);
+            }
+        }
+        return result;
+    }
 }
